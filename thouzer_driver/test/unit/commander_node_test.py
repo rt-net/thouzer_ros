b8d6cd2cd58c361689dbd4f05e411528c20e1d30
Daisuke Sato <daisuke.sato@rt-net.jp>
2021-12-07 16:08:13 +0900

    Add license notation to the test code

--- thouzer_driver/test/unit/commander_node_test.py
+++ thouzer_driver/test/unit/commander_node_test.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+# coding: utf8
+
+# SPDX-License-Identifier: Apache-2.0
+
+# Copyright 2021 RT Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is adapted from
+# https://github.com/groove-x/mqtt_bridge/blob/db4ee39da436e60fb3d4557cc1122ec822b65668/scripts/mqtt_bridge_node_test.py
+# which is released under the MIT License.
+# Copyright (c) 2016 GROOVE X, Inc.
+# https://github.com/groove-x/mqtt_bridge/blob/db4ee39da436e60fb3d4557cc1122ec822b65668/LICENSE.txt
+
+
 import logging
 import sys
 import time
